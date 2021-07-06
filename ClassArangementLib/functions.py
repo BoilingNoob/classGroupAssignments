d0d5600bc5430d6a3bbfcc94e8be309b198f625b
dasdot <dealwithit441@gmail.com>
2021-07-06 02:00:28 -0400

    aahh

--- ClassArangementLib/functions.py
+++ ClassArangementLib/functions.py
@@ -7,16 +7,25 @@ def findFactors(num):
         if(num % i == 0):
             factors.append(i)
     return factors[1:]
+
 def determinePossibleOrganizations(numberOfGroups,NumberOfObjects):
     #m = numberOfGroups       n = NumberOfObjects
     waysToAssort = math.factorial(numberOfGroups * NumberOfObjects) 
     waysToAssort /= (math.pow(math.factorial(NumberOfObjects),numberOfGroups) * math.factorial(numberOfGroups))
     return waysToAssort
+
+
 class studentGroupDay:
     studentDict = {}
     numberOfStudents = 0
     numberOfGroups = 0
     studentGroupDict = {}
+
+    def __init__(self,studentDict,numberOfGroups,studentGroupDict):
+        self.studentDict = studentDict
+        self.numberOfStudents = len(studentDict)
+        self.numberOfGroups = numberOfGroups
+        self.studentGroupDict = studentGroupDict
 
 
 class situation:
@@ -30,8 +39,17 @@ class situation:
         self.numberOfStudents = len(studentDict)
         self.numberOfGroups = numberOfGroups
 
-    def determinePossibleOrganizations(self,numberOfGroups,NumberOfObjects):
-        #m = numberOfGroups       n = NumberOfObjects
-        waysToAssort = math.factorial(numberOfGroups * NumberOfObjects) 
-        waysToAssort /= (math.pow(math.factorial(NumberOfObjects),numberOfGroups) * math.factorial(numberOfGroups))
+    def determinePossibleOrganizations(self):
+        #m = self.numberOfGroups       n = self.numberOfStudents
+        waysToAssort = math.factorial(self.numberOfGroups * self.numberOfStudents) 
+        waysToAssort /= (math.pow(math.factorial(self.numberOfStudents),self.numberOfGroups) * math.factorial(self.numberOfGroups))
         return waysToAssort
+
+    def organizeGroups(self):
+        for i in range(determinePossibleOrganizations()):
+            
+            studentGroupDict = {}#generate day groups
+
+
+            temp = studentGroupDay(self.studentDict,self.numberOfGroups,studentGroupDict)
+            self.studentGroupPlaylist.append(temp)
