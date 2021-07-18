a4e420fe3d60b5cc53fed6e0f9348f50af86c2a2
BoilingNoob <morgandowd@teksavvy.com>
2021-07-18 14:55:03 -0400

    reorganized

--- makegroupIteration.py
+++ makegroupIteration.py
@@ -4,22 +4,9 @@ import math
 from ClassArangementLib import functions as classFuncs
 import random as rand
 
-def generateGroupRawList(classList):
-    groupList = []
-    classListRandomized = []
-    classListMutable = classList
-    rand.seed()
-    for i in range(0,len(classList)):
-        randInt = rand.randrange(0,len(classListMutable))
-        groupList.append(classListMutable.pop(randInt))
-    
-    return groupList
-
-
 listOfStudents = classFuncs.getStudentList('names.txt')
+randomizedList = classFuncs.generateGroupRawListRandom(listOfStudents)
+
+
 print(listOfStudents)
-
-randomizedList = generateGroupRawList(listOfStudents)
-
 print(randomizedList)
-
