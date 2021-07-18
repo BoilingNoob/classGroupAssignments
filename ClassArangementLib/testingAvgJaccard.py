80640a5cba22e0b672a675fb5a71dd255a7432b1
BoilingNoob <morgandowd@teksavvy.com>
2021-07-18 14:21:58 -0400

    made body

--- ClassArangementLib/testingAvgJaccard.py
+++ ClassArangementLib/testingAvgJaccard.py
@@ -3,6 +3,13 @@ import hashlib as crypt
 import math
 from ClassArangementLib import functions as classFuncs
 
-def calcTotalJaccard(list):
-    
-    return 0
+def calc2JaccardVal(classItA,classItB):
+    #take two iteratiosn of groups, compare each group of list1 to list2
+    jaccardIndicies = []
+    for groupA in range(0,len(classItA)):
+        for groupB in range(0,len(classItB)):
+            jaccardIndicies.append(classFuncs.calcJaccard(groupA,groupB))
+
+    return jaccardIndicies
+
+
