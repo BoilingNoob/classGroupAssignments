c3db86c48b78984c0486fb74002b3820d9c1fbab
BoilingNoob <morgandowd@teksavvy.com>
2021-07-22 21:44:40 -0400

    added everything

--- makegroupIteration.py
+++ makegroupIteration.py
@@ -1,12 +1,37 @@
 from os import replace
 import hashlib as crypt
 import math
-from ClassArangementLib import functions as classFuncs
+#import ClassArangementLib as classFuncs
 import random as rand
+from ClassArangementLib import functions as classFuncs
 
 listOfStudents = classFuncs.getStudentList('names.txt')
 randomizedList = classFuncs.generateGroupRawListRandom(listOfStudents)
 
+def makeGroups(numberOfGroups,RandomizedStudentList):
+    listOfGroups = []
+    numberOfStudents = len(RandomizedStudentList)
+    ExtraStudents = numberOfStudents % numberOfGroups
+    studentsPerGroupEven = (numberOfStudents - (ExtraStudents) )/numberOfGroups
+    mutStudentList = RandomizedStudentList
+    
 
+    for i in range(0,numberOfGroups):
+        temproup = []
+        for j in range(0,studentsPerGroupEven):
+            temproup.append(mutStudentList.pop())
+        if(i < ExtraStudents):
+            temproup.append(mutStudentList.pop())
+
+        listOfGroups.append(temproup)
+
+    return listOfGroups
+
+"""
 print(listOfStudents)
 print(randomizedList)
+"""
+
+
+groups = makeGroups(4,randomizedList)
+print(groups[0])
